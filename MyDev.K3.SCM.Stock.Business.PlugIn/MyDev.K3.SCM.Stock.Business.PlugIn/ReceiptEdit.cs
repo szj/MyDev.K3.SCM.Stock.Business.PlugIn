25270826e0d9dbb5addcceb9848cca9dca04366b
fjfdszj <fjfdszj@163.com>
2016-07-06 16:24:46 +0800

    k3cloud测试插件

---
+++ MyDev.K3.SCM.Stock.Business.PlugIn/MyDev.K3.SCM.Stock.Business.PlugIn/ReceiptEdit.cs
@@ -0,0 +1,25 @@
+﻿using Kingdee.BOS.Core.DynamicForm;
+using Kingdee.BOS.Core.DynamicForm.PlugIn.Args;
+
+namespace MyDev.K3.SCM.Stock.Business.PlugIn
+{
+    public class ReceiptEdit : Kingdee.BOS.Core.Bill.PlugIn.AbstractBillPlugIn
+
+    {
+        public override void BarItemClick(BarItemClickEventArgs e)
+        {
+            base.BarItemClick(e);
+
+            if (e.BarItemKey == "btnTest")
+            {
+                this.View.ShowMessage("Hello world!", MessageBoxType.Notice);
+
+            }
+        }
+
+    }
+
+
+
+
+}
